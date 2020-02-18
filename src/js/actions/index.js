1f3ccfa35166340d5b4c1b527ab880cfc5c7a417
gmurray <gregory.murray@advent.com>
2020-02-18 11:23:58 -0500

    up to redux-saga

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,5 +1,19 @@
-import { ADD_ARTICLE } from '../constants/action-types';
-
+import { ADD_ARTICLE, DATA_REQUESTED } from '../constants/action-types';
 export function addArticle(payload) {
-    return {type: ADD_ARTICLE, payload}
+    return { type: ADD_ARTICLE, payload }
+}
+
+// replaced with redux-saga
+// export function getData() {
+//     return function (dispatch) {
+//         return fetch('https://jsonplaceholder.typicode.com/posts')
+//             .then(response => response.json())
+//             .then(json => {
+//                 dispatch({ type: DATA_LOADED, payload: json });
+//             });
+//     };
+// }
+
+export function getData() {
+    return { type: DATA_REQUESTED };
 }
